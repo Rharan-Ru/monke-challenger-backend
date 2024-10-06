13ac2909a3b82919a4422b1017c5ecd47f66ac18
Rharan-Ru <rharanru@gmail.com>
2024-10-06 04:39:42 -0300

    chore: Update app controller to use health endpoint instead of hello endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
+// nestJs module
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+// nestJs controllers/services
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompaniesModule } from './companies/companies.module';
+// nestJs application modules
+import { CompaniesModule } from './application/companies/companies.module';
+import { UserModule } from './application/user/user.module';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { JwtAuthGuard } from './infrastructure/auth/jwt-auth.guard';
+import { DatabaseModule } from './infrastructure/database/database.module';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CompaniesModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
